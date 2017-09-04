2929e319c738b45fddcea15ffa3d62105d750df8
Jason Schafer <jason59549@gmail.com>
2017-09-04 14:51:53 -0400

    InitialPlatform Script
    Adds a couple of fixes,
    Originally obtained from:
    https://www.youtube.com/watch?v=bfNyb4ep0Gw
    http://turtlescripts.com/project/gjdgyu-Easy-Platform-Builder/

---
+++ platform.lua
@@ -0,0 +1,70 @@
+local tArgs = {...}
+local length = tonumber(tArgs[1]) 
+local width = tonumber(tArgs[2])
+local turnRight = true
+local slot = 1
+
+
+
+--Helper Functions
+function nextSlot()
+	if slot < 16
+		return (slot = slot + 1)
+	else
+		return slot = 1
+	end
+end
+
+if width == nil then
+    print("1. Place turtle facing direction of said platform on left side.")
+    print("2. Load turtle with materials for the platform.")
+    print("3. Type 'platform <length> <width>'")
+end
+
+turtle.select(slot)
+
+turtle.dig()
+turtle.forward()
+
+for j = 1, width, 1 do
+
+  for i = 1, length, 1 do
+    if turtle.getItemCount(slot) == 0 then
+      nextSlot()
+      turtle.select(slot)
+      turtle.digDown()
+      turtle.placeDown()
+    else
+        if not turtle.refuel() then
+			turtle.digDown()
+			turtle.placeDown()
+		end
+    end
+    
+    if i < length then
+        turtle.dig()
+      turtle.forward()
+    end
+  
+  end
+  
+  if j < width then
+  
+    if turnRight == true then
+      turtle.turnRight()
+      turtle.dig()
+      turtle.forward()
+      turtle.turnRight()
+      turnRight = false
+    else
+      turtle.turnLeft()
+      turtle.dig()
+      turtle.forward()
+      turtle.turnLeft()
+      turnRight = true
+    end
+    
+  end
+  
+end
+
