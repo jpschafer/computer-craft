1b2ee27ebfc64a2200f67832198be05129fd1a00
Jason Schafer <jason59549@gmail.com>
2022-10-02 20:47:31 -0400

    Fixed Basic Miner
    Basic Miner Doesn't have any real error handling for fuel,
    but we can eventually fix this to make it wait for more fuel.

--- miner.lua
+++ miner.lua
@@ -1,7 +1,7 @@
-local tArgs = {...}
-local length = tonumber(tArgs[1]) 
+local tArgs = { ... }
+local length = tonumber(tArgs[1])
 local width = tonumber(tArgs[2])
-local width = tonumber(tArgs[3])
+local height = tonumber(tArgs[3])
 local turnRight = true
 local slot = 1
 
@@ -12,7 +12,7 @@ function nextSlot()
 	else
 		slot = 1
 	end
-      turtle.select(slot)
+	turtle.select(slot)
 end
 
 function place()
@@ -25,78 +25,78 @@ function place()
 	end
 end
 
-if width == nil then
-    print("1. Place turtle (with pickaxe) facing direction of said mining (cube) area on left side and above the first layer you want to mine.")
-    print("2. Load turtle with enough fuel for mining.")
-    print("3. Type 'platform <length> <width> <height>'")
-	return 0
-end
+
 
 -- Main Function --
+
 function main()
-		
+	
+	if width == nil then
+		print("1. Place turtle (with pickaxe) facing direction of said mining (cube) area on left side and above the first layer you want to mine.")
+		print("2. Load turtle with enough fuel for mining.")
+		print("3. Type 'platform <length> <width> <height>'")
+		return 0
+	end
+	
+	turtle.select(slot)
+	turtle.refuel();
+
+	turtle.dig()
+	turtle.forward()
+
+	-- For Every 3 Levels of Height
+	for k = 0, height, 3 do
+		-- For Every block of width
+		for j = 1, width, 1 do
+
+			-- For every block of length
+			for i = 1, length, 1 do
+				if turtle.getItemCount(slot) == 0 then
+					nextSlot()
+				else
+					turtle.refuel()
+				end
+
+				turtle.digUp()
+				turtle.digDown()
+
+				if i < length then
+					turtle.dig()
+					turtle.forward()
+				end
+			end
+
+			if j < width then
+
+				if turnRight == true then
+					turtle.turnRight()
+					turtle.dig()
+					turtle.forward()
+					turtle.turnRight()
+					turnRight = false
+				else
+					turtle.turnLeft()
+					turtle.dig()
+					turtle.forward()
+					turtle.turnLeft()
+					turnRight = true
+				end
+			end
+		end
+
+		if k < height then
+			turtle.digUp()
+			turtle.up()
+			turtle.digUp()
+			turtle.up()
+			turtle.digUp()
+			turtle.up()
+			turtle.turnRight()
+			turtle.turnRight()
+
+			-- due to orientation magic, we don't need to 'not' our turnRight variable
+		end
+	end
 end
 
-
-turtle.select(slot)
-
-turtle.dig()
-turtle.forward()
-
--- For Every 3 Levels of Height
-for k = 1, length, 3 do
-  -- For Every block of width
-  for j = 1, width, 1 do
-
-    -- For every block of length
-    for i = 1, length, 1 do
-      if turtle.getItemCount(slot) == 0 then
-        nextSlot()
-        turtle.digDown()
-        turtle.digUp()
-      else
-        if not turtle.refuel() then
-          turtle.digDown()
-          turtle.digUp()
-      end
-    end
-
-      if i < length then
-        turtle.dig()
-        turtle.forward()
-      end
-
-    end
-
-    if j < width then
-
-      if turnRight == true then
-        turtle.turnRight()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnRight()
-        turnRight = false
-      else
-        turtle.turnLeft()
-        turtle.dig()
-        turtle.forward()
-        turtle.turnLeft()
-        turnRight = true
-      end
-    end
-  end
-  
-  if k < height then
-    turtle.digUp()
-    turtle.up()
-    turtle.digUp()
-    turtle.up()
-    turtle.digUp()
-    turtle.up()
-    turtle.turnRight()
-    turtle.turnRight()
-    
-    -- Set turnRight Boolean to Opposite to retrace path to beginning
-    turnRight = not turnRight
-  end
-end
+main()
